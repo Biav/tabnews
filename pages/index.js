1b549d272dffad44b71fd5b70dec216c69a95a40
Biav <beatrizvieirasilva.v@gmail.com>
2024-02-09 11:32:29 +0000

    Adding Constructing Page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
 const Home = () => {
-  return <div>Test NextJS</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        padding: "3rem",
+      }}
+    >
+      <h1>
+        Constructing a page of news using NextJS, to study a bunch of concepts
+        of developing like BD, backend, deploy and test
+      </h1>
+      <img
+        src="https://www.drupal.org/files/project-images/nextjs-icon-dark-background.png"
+        alt="NextJS logo"
+        style={{ width: "200px" }}
+      />
+    </div>
+  );
 };
 
 export default Home;
