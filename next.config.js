2a7d586bc6f64d8bb6009a14660a9fd9d7648e89
Biav <beatrizvieirasilva.v@gmail.com>
2024-03-11 23:18:11 +0000

    feat: removing file

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  webpack: (config, { webpack }) => {
-    config.plugins.push(new webpack.IgnorePlugin(/\/src\/tests\//)); // <<<<<====== This line solved my issue
-
-    return config;
-  },
-};
