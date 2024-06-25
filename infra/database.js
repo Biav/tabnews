985a4f480be46f1b734b7e9e2d2fccdaa77fe841
Biav <beatrizvieirasilva.v@gmail.com>
2024-06-25 23:46:16 +0000

    feat: adding ssl to connection

--- infra/database.js
+++ infra/database.js
@@ -9,7 +9,10 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: String(process.env.POSTGRES_PASSWORD),
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
+
+  console.log(process.env);
 
   try {
     await client.connect();
