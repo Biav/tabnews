93517fb8153f2c9e51000a4529efab4a1fa3f739
Biav <beatrizvieirasilva.v@gmail.com>
2024-08-27 22:56:19 +0000

    feat: adding post to migrations

---
+++ __tests__/api/migrations/post.test.js
@@ -0,0 +1,22 @@
+import query from "infra/database";
+
+beforeAll(async () => {
+  await cleanSchema();
+});
+
+async function cleanSchema() {
+  await query("drop schema public cascade; create schema public;");
+}
+
+describe("Should test migrations endpoint", () => {
+  it("POST /api/migrations/v1", async () => {
+    const response = await fetch("http://localhost:3000/api/migrations/v1", {
+      method: "POST",
+    });
+
+    const responseBody = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(responseBody.length).toBeGreaterThan(0);
+  });
+});
--- pages/api/migrations/v1/index.js
+++ pages/api/migrations/v1/index.js
@@ -2,11 +2,22 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
+  const methodsAlloweds = ["GET", "POST"];
+
+  if (!methodsAlloweds.includes(request.method)) {
+    return response.status(405).json("Method not allowed");
+  }
+
+  const dryRun = request.method === "POST" ? true : false;
+
   const migrations = await migrationRunner({
     databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
+    dryRun,
     dir: join("infra", "migrations"),
     direction: "up",
+    verbose: "true",
+    migrationsTable: "pgmigrations",
   });
+
   response.status(200).json(migrations);
 }
