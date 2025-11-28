e52d0d1eb6a8711ba9b7d043ca048f1d64f11b2e
Biav <beatrizvieirasilva.v@gmail.com>
2025-11-28 17:10:28 +0000

    refactor: tests

--- __tests__/api/migrations/get.test.js
+++ __tests__/api/migrations/get.test.js
@@ -10,8 +10,8 @@ async function cleanSchema() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
-describe("Should test migrations endpoint", () => {
-  it("GET /api/migrations/v1", async () => {
+describe("GET /api/migrations/v1", () => {
+  it("Should get the migrations", async () => {
     const response = await fetch("http://localhost:3000/api/migrations/v1");
 
     const responseBody = await response.json();
--- __tests__/api/migrations/post.test.js
+++ __tests__/api/migrations/post.test.js
@@ -10,24 +10,26 @@ async function cleanSchema() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
-describe("Should test migrations endpoint", () => {
-  it("POST /api/migrations/v1", async () => {
-    const response = await fetch("http://localhost:3000/api/migrations/v1", {
-      method: "POST",
+describe("POST /api/migrations/v1", () => {
+  describe("Anonymous user", () => {
+    it("should retrieve the pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/migrations/v1", {
+        method: "POST",
+      });
+
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(201);
+      expect(responseBody.length).toBeGreaterThan(0);
+
+      const response2 = await fetch("http://localhost:3000/api/migrations/v1", {
+        method: "POST",
+      });
+
+      const responseBody2 = await response2.json();
+
+      expect(response2.status).toBe(200);
+      expect(responseBody2.length).toBe(0);
     });
-
-    const responseBody = await response.json();
-
-    expect(response.status).toBe(201);
-    expect(responseBody.length).toBeGreaterThan(0);
-
-    const response2 = await fetch("http://localhost:3000/api/migrations/v1", {
-      method: "POST",
-    });
-
-    const responseBody2 = await response2.json();
-
-    expect(response2.status).toBe(200);
-    expect(responseBody2.length).toBe(0);
   });
 });
