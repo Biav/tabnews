9af028f59f9ffe50ba9f8726bd678367b5c62638
Biav <beatrizvieirasilva.v@gmail.com>
2026-02-02 11:52:47 +0000

    fix: remove unused import

--- infra/controller/index.ts
+++ infra/controller/index.ts
@@ -1,6 +1,5 @@
 import { InternalServerError } from "infra/errors";
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { NextHandler } from "next-connect";
 
 function onErrorHandler(err: Error, req: NextApiRequest, res: NextApiResponse) {
   const cause = err;
