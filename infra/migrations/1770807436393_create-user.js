3c17ad293fe1c2dfeb58b62446cda32f57da5726
Biav <beatrizvieirasilva.v@gmail.com>
2026-03-20 11:52:46 +0000

    fix: update file type

--- infra/migrations/1770807436393_create-user.js
+++ infra/migrations/1770807436393_create-user.js
@@ -1,14 +1,14 @@
 /**
  * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
  */
-export const shorthands = undefined;
+exports.shorthands = undefined;
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {
+exports.up = (pgm) => {
   pgm.createTable("users", {
     id: {
       type: "uuid",
@@ -46,4 +46,4 @@ export const up = (pgm) => {
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {};
+exports.down = (pgm) => {};
